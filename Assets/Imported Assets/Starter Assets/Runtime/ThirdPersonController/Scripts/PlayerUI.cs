fe607e7e9174ea09404f678bc02ac308e1dbcd2d
송신화 <bruno0629@naver.com>
2025-04-27 00:21:57 +0900

    스킬 쿨타임 추가
    - UI 표시

--- Assets/Imported Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/PlayerUI.cs
+++ Assets/Imported Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/PlayerUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,8 +6,14 @@ public class PlayerUI : MonoBehaviour
 {
     public Image hpBar;
     public Image spBar;
+    public Image imgSkillCooldown;
 
     public PlayerStatus playerStatus;
+
+    private void Start()
+    {
+        imgSkillCooldown.fillAmount = 0f;
+    }
 
     public void UpdateUI()
     {
--- Assets/Imported Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/SkillController.cs
+++ Assets/Imported Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/SkillController.cs
@@ -7,6 +7,11 @@ public class SkillController : MonoBehaviour
 {
     private ThirdPersonController _thirdPersonController;
     private PlayerStatus _playerStatus;
+    public PlayerUI playerUI;
+    public float skillCooldownTime = 10f;
+    public float currentSkillCooldown;
+    private bool isCooldown = false;
+    
     Animator animator;
 
     void Start()
@@ -18,9 +23,25 @@ public class SkillController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        if (isCooldown)
         {
-            if (_thirdPersonController.canMove && _playerStatus.currentSp >= 20)
+            currentSkillCooldown -= Time.deltaTime;
+
+            if (currentSkillCooldown <= 0f)
+            {
+                isCooldown = false;
+                playerUI.imgSkillCooldown.fillAmount = 0;
+                Debug.Log("스킬 사용 가능");
+            }
+            else
+            {
+                playerUI.imgSkillCooldown.fillAmount = currentSkillCooldown / skillCooldownTime;
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.C) && !isCooldown)
+        {
+            if (_thirdPersonController.canMove && _playerStatus.currentSp >= 30)
             {
                 _thirdPersonController.canMove = false;
                 skillActivate();
@@ -30,6 +51,10 @@ public class SkillController : MonoBehaviour
 
     void skillActivate()
     {
+        isCooldown = true;
+        currentSkillCooldown = skillCooldownTime;
+        playerUI.imgSkillCooldown.fillAmount = 1;
+        
         animator.SetTrigger("skill");
         _playerStatus.SkillStamina();
         Debug.Log("Weapon Skill Activated");
